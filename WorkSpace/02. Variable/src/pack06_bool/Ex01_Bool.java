0d64ba56d6f3fdb5302f197a4f605922048f3a9a
Kwakyoung <128439473+Kwakyoung@users.noreply.github.com>
2023-03-31 15:20:42 +0900

    자바 수업자료 업로드

---
+++ WorkSpace/02. Variable/src/pack06_bool/Ex01_Bool.java
@@ -0,0 +1,20 @@
+package pack06_bool;
+
+public class Ex01_Bool {
+	public static void main(String[] args) {
+		//부울형(논리형) : boolean
+		// true , false 만 기억 -> 조건식 작성 시 사용.
+		// 컴퓨터는 어떠한 판단을 true 와 false로만 함.
+		// 컴퓨터에게 어떠한 질문을 할때는 true , false로 답이 나오는것만 가능
+		// 예시) 내 이름이 뭐게? x
+		// 예시) 오늘 온도가 높은지? x 
+		// 예시) 오늘 온도가 15도 보다 높습니까? o
+		boolean t = true ;
+		boolean f = false ;
+		// 문자열도 표현하고싶고 변수도 표현하고싶다? 더하기로 가능
+		System.out.println("변수 t에 들어있는 값 : " + t);
+		// 변수 f에 들어있는 값? 표현해보기
+		System.out.println("변수 f에 들어있는 값 : " + f);
+	}
+
+}
---
+++ WorkSpace/999.Test/src/Test00.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test00 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String Data = sc.nextLine();
+		System.out.println(Data);
+		
+		double sum = Double.parseDouble(Data)+10;
+		System.out.println(sum);
+		
+	}
+}
